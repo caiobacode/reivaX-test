0f65303996051a660946b7387e7bc601fc152146
caiobacode <caioodsf@gmail.com>
2023-05-26 17:25:34 -0300

    chore: configurando conexao ao socket.io

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,34 @@
-function App() {
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
+
+const App = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:5000/api', {
+      extraHeaders: {
+        'X-Username': 'user',
+        'X-Password': 'user'
+      }
+    });
+    socket.on('connect', () => {
+      console.log('Conectado ao servidor Socket.IO');
+    });
+
+    // atende evento "credentials" e printa os tokens
+    socket.on('credentials', (data) => {
+      console.log('Tokens:', data);
+    })
+
+    // atende evento "data" e printa os dados
+    socket.on('data', (data) => {
+      console.log('data:', data);
+    })
+
+    return () => {
+      socket.disconnect(); // Desconecta quando o componente for desmontado
+    };
+  }, [])
+
+
   return (
     <div className="App">
       ReivaX
