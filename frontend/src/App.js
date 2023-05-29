a0f695233e6886886612af43f62487462f24766a
caiobacode <caioodsf@gmail.com>
2023-05-29 17:11:52 -0300

    feat: criando logica para atualizar tokens a cada 450 segundos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import io from 'socket.io-client';
 import { Login, Home } from './pages';
-import { selectUser, setData, selectTable, setClearTable } from './redux';
+import { selectUser, setData, selectTable, setClearTable, changePage } from './redux';
 import { getLocalStorage, setLocalStorage } from './utils/localStorage';
 
 const App = () => {
@@ -37,14 +37,27 @@ const App = () => {
 
     const token = getLocalStorage('token', false);
     if (clearTable === true) {
-      socket.emit('clear', { token });
+      socket.emit('clear', { token }, (response) => {
+        console.log('Retorno do comando "clear":', response);
+      });
       dispatch(setData([]));
+      dispatch(changePage(1))
       setTimeout(() => {
         dispatch(setClearTable(false));
       }, 500)
     }
 
+    // a cada 445 segundos, faremos uma requisicao para atualizar os tokens
+    const interval = setInterval(() => {
+      const refreshToken = getLocalStorage('refresh-token', false)
+      socket.emit('refresh_tokens', { token: refreshToken }, (response) => {
+        setLocalStorage('token', response.access_token);
+        setLocalStorage('refresh-token', response.refresh_token)
+      });
+    }, 450000);
+
     return () => {
+      clearInterval(interval);
       socket.disconnect(); // Desconecta quando o componente for desmontado
     };
   }, [user, clearTable]);
