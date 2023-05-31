02338ab50a35193cee624b0100cb12a7a619035e
caiobacode <caioodsf@gmail.com>
2023-05-31 02:58:41 -0300

    feat: estiliza tabela

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -2,36 +2,35 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectTable } from '../redux/tableSlice';
 import { getDataFromActualPage } from '../utils';
+import '../style/Table.css'
 
 export default function Table() {
   const { data, actualPage } = useSelector(selectTable);
   const dataFromActualPage = getDataFromActualPage(data, actualPage);
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Param1</th>
-            <th>Param2</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            dataFromActualPage?.map((d, index) => {
-              return (
-                <tr key={index}>
-                  <td>{d.type}</td>
-                  <td>{d.param1}</td>
-                  <td>{d.param2}</td>
-                  <td>{d.timestamp}</td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
-    </div>
+    <table className='content-table'>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Param1</th>
+          <th>Param2</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          dataFromActualPage?.map((d, index) => {
+            return (
+              <tr key={index}>
+                <td className='type-td'>{d.type}</td>
+                <td>{d.param1}</td>
+                <td>{d.param2}</td>
+                <td>{d.timestamp}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Table, UserOptions } from '../components';
+import '../style/Home.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className='home-div'>
       <UserOptions />
       <Table />
     </div>
