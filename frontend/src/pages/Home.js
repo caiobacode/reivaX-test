c3327c61d8318c09a428470e303f85d872988417
caiobacode <caioodsf@gmail.com>
2023-05-31 22:06:33 -0300

    refactor: formata erros de sintaxe dos componentes "pages"

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Table, UserOptions } from '../components';
-import '../style/Home.css'
-import Filters from '../components/Filters';
 import { useSelector } from 'react-redux';
+import { Table, UserOptions } from '../components';
+import '../style/Home.css';
+import Filters from '../components/Filters';
 import { selectFilters } from '../redux/filtersSlice';
 
 export default function Home() {
   const { showFiltersWindow } = useSelector(selectFilters);
   return (
-    <div className='home-div'>
+    <div className="home-div">
       {
         showFiltersWindow && <Filters />
       }
-      <div className='home-content'>
+      <div className="home-content">
         <UserOptions />
         <Table />
       </div>
     </div>
-  )
+  );
 }
